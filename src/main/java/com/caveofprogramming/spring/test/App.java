be9241ed9f8a436efec9bef5a9d28873c5606599
raghu <raghu.bogimi@gmail.com>
2016-08-12 00:59:37 -0500

    added try catch

--- src/main/java/com/caveofprogramming/spring/test/App.java
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -4,16 +4,23 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class App {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"com/caveofprogramming/spring/test/beans/beans.xml");
+
 		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
-		List<Offer> offers = offersDao.getOffer();
-		for (Offer offer : offers) {
-			System.out.println(offer);
+
+		try {
+			List<Offer> offers = offersDao.getOffer();
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+		} catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
 		}
 
 		((ClassPathXmlApplicationContext) context).close();
